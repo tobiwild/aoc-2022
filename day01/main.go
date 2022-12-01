1ccfbc1528019a598ec8a253f2f9fdc667ed0725
Tobias Wild <tobiwild@web.de>
2022-12-01 09:24:00 +0100

    day01

---
+++ day01/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func run() error {
+	readFile, err := os.Open("input.txt")
+
+	if err != nil {
+		return err
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	defer readFile.Close()
+
+	var maxSums [3]int
+	currentSum := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		if line == "" {
+			for i, maxSum := range maxSums {
+				if currentSum > maxSum {
+					maxSums[i] = currentSum
+					break
+				}
+			}
+			currentSum = 0
+			continue
+		}
+
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			return err
+		}
+		currentSum += number
+	}
+	for i, maxSum := range maxSums {
+		if currentSum > maxSum {
+			maxSums[i] = currentSum
+			break
+		}
+	}
+
+	fmt.Printf("Part 1 result: %v\n", maxSums[0])
+
+	var totalSum int
+	for _, maxSum := range maxSums {
+		totalSum += maxSum
+	}
+	fmt.Printf("Part 2 result: %v\n", totalSum)
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
